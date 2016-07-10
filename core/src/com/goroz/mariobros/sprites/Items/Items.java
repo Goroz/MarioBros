56850f8f1eed0de512d92457324e5620300035e9
goroz <sgoroz1234@gmail.com>
2016-07-10 15:39:30 +0200

    Added class items mushroom.png is packed into Mario_and_Enemies.png

---
+++ core/src/com/goroz/mariobros/sprites/Items/Items.java
@@ -0,0 +1,54 @@
+package com.goroz.mariobros.sprites.Items;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+import com.goroz.mariobros.MarioBros;
+import com.goroz.mariobros.screen.PlayScreen;
+
+/**
+ * Created by HC Lim on 10/7/2016.
+ */
+
+public abstract class Items extends Sprite {
+
+    protected PlayScreen screen;
+    protected World world;
+    protected Vector2 velocity;
+    private boolean setToDestroyed;
+    private boolean destroyed;
+    private Body body;
+
+    public Items(PlayScreen screen, float x, float y) {
+        this.screen = screen;
+        this.world = screen.getWorld();
+        setPosition(x, y);
+        setBounds(getX(), getY(), MarioBros.TILE_SIZE / MarioBros.PPM, MarioBros.TILE_SIZE / MarioBros.PPM);
+        defineItem();
+        setToDestroyed = false;
+        destroyed = false;
+    }
+
+    protected abstract void defineItem();
+
+    public abstract void use();
+
+    public void update(float dt) {
+        if (setToDestroyed && !destroyed) {
+            world.destroyBody(body);
+            destroyed = true;
+        }
+    }
+
+    public void draw(Batch batch) {
+        if (!destroyed) {
+            super.draw(batch);
+        }
+    }
+
+    public void destroy() {
+        setToDestroyed = true;
+    }
+}
